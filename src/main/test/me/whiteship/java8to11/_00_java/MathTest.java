dcd85aff40e6403281ca9501f985130cb971e66e
minhee.son <minhee4735@gmail.com>
2023-03-12 09:45:25 +0900

    feat Math 라이브러리 사용법 정리, ArrayTest 추가

---
+++ src/main/test/me/whiteship/java8to11/_00_java/MathTest.java
@@ -0,0 +1,84 @@
+package me.whiteship.java8to11._00_java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html
+ * */
+public class MathTest {
+
+    @Test
+    void abs() {
+        assertThat(Math.abs(10)).isEqualTo(10);
+        assertThat(Math.abs(-10)).isEqualTo(10);
+    }
+
+    @Test
+    void ceil() {
+        assertThat(Math.ceil(10.123)).isEqualTo(11);
+        assertThat(Math.ceil(10.56)).isEqualTo(11);
+    }
+
+    @Test
+    void floor() {
+        assertThat(Math.floor(10.39)).isEqualTo(10);
+        assertThat(Math.floor(10.56)).isEqualTo(10);
+    }
+
+    @Test
+    void round() {
+        assertThat(Math.round(10.39)).isEqualTo(10);
+        assertThat(Math.round(10.56)).isEqualTo(11);
+    }
+
+    @Test
+    void max() {
+        assertThat(Math.max(10, 100)).isEqualTo(100);
+    }
+
+    @Test
+    void min() {
+        assertThat(Math.min(10, 100)).isEqualTo(10);
+    }
+
+    @Test
+    void pow() {
+        assertThat(Math.pow(2, 4)).isEqualTo(16);
+    }
+
+    @Test
+    void sqrt() {
+        assertThat(Math.sqrt(25)).isEqualTo(5);
+        assertThat(Math.sqrt(26)).isEqualTo(5.0990195135927845);
+    }
+
+    @Test
+    void addExact() {
+        assertThat(Math.addExact(10, 30)).isEqualTo(40);
+        assertThat(Math.addExact(30, 10)).isEqualTo(40);
+    }
+
+    @Test
+    void subtractExact() {
+        assertThat(Math.subtractExact(10, 30)).isEqualTo(-20);
+        assertThat(Math.subtractExact(30, 10)).isEqualTo(20);
+    }
+
+    @Test
+    void decrementExact() {
+        assertThat(Math.decrementExact(10)).isEqualTo(9);
+        assertThat(Math.decrementExact(318)).isEqualTo(317);
+    }
+
+    @Test
+    void multiplyExact() {
+        assertThat(Math.multiplyExact(10, 20)).isEqualTo(200);
+    }
+
+    @Test
+    void toIntExact() {
+        assertThat(Math.toIntExact(128902389L)).isEqualTo(128902389);
+    }
+}
---
+++ src/main/test/me/whiteship/java8to11/_05_stream/ArrayTest.java
@@ -0,0 +1,16 @@
+package me.whiteship.java8to11._05_stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ArrayTest {
+
+    @Test
+    void 배열의_각_요소에_2를_곱한_배열을_반환한다() {
+        int[] arr = {1, 20, 100, -50, 0};
+        assertThat(Arrays.stream(arr).map(i -> i*2).toArray()).isEqualTo(new int[]{2, 40, 200, -100, 0});
+    }
+}
